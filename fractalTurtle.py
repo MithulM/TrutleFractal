419ee36d73c6a12cfcbd6e3f60b463349ce3fc12
Mithul Manivannan <36342151+MithulM@users.noreply.github.com>
2022-11-14 12:29:11 -0600

    Created recursive call for Lgrammar

--- fractalTurtle.py
+++ fractalTurtle.py
@@ -1,35 +1,59 @@
 from turtle import *
 
-if __name__ == "__main__":
-    t = Turtle()
-    t.speed(0)
-    t.pencolor('red')
-    t.hideturtle()
-    gen = 4
-    scale = 0.5
-    windowSize = 500
-    size = windowSize * scale ** (gen - 1)
-    commands = "[Y]++[Y]++[Y]++[Y]++[Y]"
-    Lgrammar = {
-        "F": "",
-        "f": "",
-        "X": "UF++VF----YF[-UF----XF]++",
-        "Y": "+UF--VF[---XF--YF]+",
-        "U": "-XF++YF[+++UF++VF]-",
-        "V": "--UF++++XF[+VF++++YF]--YF",
-    }
-    rot = 36
-    for a in range(gen-1):
+
+def recLgrammar(t, axiom, angle, gen, size, states=[], **Lgrammar):
+    for command in axiom:
+        if gen:
+            match command:
+                case "X" | "Y" | "U" | "V" | "F" | "f":
+                    recLgrammar(t, Lgrammar[command], angle,
+                                gen - 1, size, states, ** Lgrammar)
+                case "[":
+                    states.append((*t.pos(), t.heading()))
+                case "]":
+                    x, y, h = states.pop()
+                    t.penup()
+                    t.setpos(x, y)
+                    t.setheading(h)
+                    t.pendown()
+                case "+":
+                    t.right(angle)
+                case "-":
+                    t.left(angle)
+                case _:
+                    pass
+        else:
+            match command:
+                case "F":
+                    t.forward(size)
+                case "f":
+                    t.penup()
+                    t.forward(size)
+                    t.pendown()
+                case "[":
+                    states.append((*t.pos(), t.heading()))
+                case "]":
+                    x, y, h = states.pop()
+                    t.penup()
+                    t.setpos(x, y)
+                    t.setheading(h)
+                    t.pendown()
+                case "+":
+                    t.right(angle)
+                case "-":
+                    t.left(angle)
+                case _:
+                    pass
+
+
+def iterLgrammar(t, axiom, angle, gen, size, **Lgrammar):
+    for _ in range(gen):
         temp = ""
-        for command in commands:
+        for command in axiom:
             temp += Lgrammar[command] if command in Lgrammar else command
-        commands = temp
-        print(commands)
+        axiom = temp
     states = []
-    t.penup()
-    t.setpos(250, 0)
-    t.pendown()
-    for command in commands:
+    for command in axiom:
         match command:
             case "X" | "Y" | "U" | "V":
                 pass
@@ -48,9 +72,48 @@ if __name__ == "__main__":
                 t.setheading(h)
                 t.pendown()
             case "+":
-                t.right(rot)
+                t.right(angle)
             case "-":
-                t.left(rot)
+                t.left(angle)
             case _:
                 print("Invalid command.")
+
+
+if __name__ == "__main__":
+    t = Turtle()
+    t.speed(0)
+    t.pencolor('red')
+    t.hideturtle()
+    initialHeading = 0
+
+    # Lgrammar values
+    gen = 4
+    scale = 1 / 3
+    windowSize = 500
+    size = windowSize * scale ** (gen)
+    axiom = "F++F++F"
+    Lgrammar = {
+        "F": "F-F++F-F",
+        "f": "",
+        "X": "",
+        "Y": "",
+        "U": "",
+        "V": "",
+    }
+    angle = 60
+
+    # Test recursive call for Lgrammar
+    t.setheading(initialHeading)
+    t.penup()
+    t.setpos(-windowSize, 0)
+    t.pendown()
+    iterLgrammar(t, axiom, angle, gen, size, **Lgrammar)
+
+    # Test recursive call for Lgrammar
+    t.setheading(initialHeading)
+    t.penup()
+    t.setpos(windowSize, 0)
+    t.pendown()
+    recLgrammar(t, axiom, angle, gen, size, **Lgrammar)
+
     done()
