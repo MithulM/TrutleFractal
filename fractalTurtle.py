7ad8153e3d0108af5678b8c590be600fd8adc338
Mithul Manivannan <36342151+MithulM@users.noreply.github.com>
2022-11-14 19:22:24 -0600

    Update fractalTurtle.py

--- fractalTurtle.py
+++ fractalTurtle.py
@@ -7,7 +7,7 @@ def recLgrammar(t, axiom, angle, gen, size, states=[], **Lgrammar):
             match command:
                 case "X" | "Y" | "U" | "V" | "F" | "f":
                     recLgrammar(t, Lgrammar[command], angle,
-                                gen - 1, size, states, ** Lgrammar)
+                                gen - 1, size, states, **Lgrammar)
                 case "[":
                     states.append((*t.pos(), t.heading()))
                 case "]":
@@ -84,23 +84,23 @@ if __name__ == "__main__":
     t.speed(0)
     t.pencolor('red')
     t.hideturtle()
-    initialHeading = 0
 
     # Lgrammar values
-    gen = 4
-    scale = 1 / 3
+    initialHeading = 0
+    gen = 3
+    scale = 1 / 2
     windowSize = 500
     size = windowSize * scale ** (gen)
-    axiom = "F++F++F"
+    axiom = "X"
     Lgrammar = {
-        "F": "F-F++F-F",
+        "F": "F",
         "f": "",
-        "X": "",
-        "Y": "",
+        "X": "X+YF+",
+        "Y": "-FX-Y",
         "U": "",
         "V": "",
     }
-    angle = 60
+    angle = 90
 
     # Test recursive call for Lgrammar
     t.setheading(initialHeading)
